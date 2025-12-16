8f0a106047ad9434b18080fe70d3ce1c6bd76aba
vanshika8484 <vanshikaagarwal8484@gmail.com>
2025-12-16 17:06:56 +0530

    Update Vercel and Vite configuration

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,9 +6,15 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),    tailwindcss(),
 ],
-base: './',
-  build: {
+build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
   }
 })
